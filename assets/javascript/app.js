8a43034a131079689467d0a5c64d9ea0076ef087
KaLaUnlimited <KLasode@gmail.com>
2017-12-19 12:33:11 -0500

    timer updated for each question

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,8 @@
 
-var timer;
-
+//var timer;
+var number = 5;
+var intervalId;
+var miliseconds=5000;
 
 $(".question1").hide();
 $(".question2").hide();
@@ -16,31 +18,36 @@ $("#start").hide();
 
 
 //timer function will hide first question and show answer page
-
+timer();
 $(".question1").show()
 //countdown begins to answer question #1
-var hide1=setTimeout(q1countDown,3000);
+
+var hide1=setTimeout(q1countDown,6000);
 
 function q1countDown(){
 	
 	$(".question1").hide()
+	number =6;
+	timer();
 	$(".question2").show()
 	// countdown begins to answer question #2
-	var hide2=setTimeout(q2countDown,3000);
-	function q2countDown(){
-	$(".question2").hide();
-	$(".question3").show();
-	//countdown begins to answer question #3
-	var hide3=setTimeout(q3countDown,3000);
-	function q3countDown(){
-	$(".question3").hide();
-	$(".question4").show();
-	//countdown begins to answer question #4
-	var hide3=setTimeout(q4countDown,3000);
-	function q4countDown(){
-	$(".question4").hide();
-	//code to display code
-	$("#score").show();
+	var hide2=setTimeout(q2countDown,6000);
+		function q2countDown(){
+			 number =6;
+			timer();
+			$(".question2").hide();
+			$(".question3").show();
+			//countdown begins to answer question #3
+			var hide3=setTimeout(q3countDown,6000);
+				function q3countDown(){
+					$(".question3").hide();
+					$(".question4").show();
+					//countdown begins to answer question #4
+					var hide3=setTimeout(q4countDown,6000);
+						function q4countDown(){
+							$(".question4").hide();
+							//code to display code
+								$("#score").show();
 	
 
 
@@ -55,17 +62,30 @@ function q1countDown(){
 
 
 //this is the timer function
-function timer(){
+  function timer() {
+      intervalId = setInterval(decrement, 1000);
+    }
 
-	var count =5;
+    function decrement() {
+    
+      number--;
 
-	setTimeout(function(){
+      $(".timer").html("You have : " + number + " seconds!");
 
-		count--;
-	$(".timer").text(count);
-	},5000)
+      if (number === 0) {
 
-}
+        stop();
+
+      //  alert("Time Up!");
+      }
+    }
+
+    function stop() {
+
+      clearInterval(intervalId);
+    }
+
+    
 
 
 
