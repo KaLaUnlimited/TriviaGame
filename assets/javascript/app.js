2c6863bf6dc3ad2ba1ff97c3c97d935389d6f82b
KaLaUnlimited <KLasode@gmail.com>
2017-12-18 21:51:47 -0500

    added another timer for score page

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,12 +1,12 @@
 
-
+var timer;
 
 
 $(".question1").hide();
 $(".question2").hide();
 $(".question3").hide();
 $(".question4").hide();
-
+$("#score").hide();
 
 
 
@@ -17,24 +17,35 @@ $("#start").hide();
 
 //timer function will hide first question and show answer page
 
-$(".question1").show();
+$(".question1").show()
+//countdown begins to answer question #1
 var hide1=setTimeout(q1countDown,3000);
 
 function q1countDown(){
 	
 	$(".question1").hide()
 	$(".question2").show()
-	// count down to question #3
-var hide2=setTimeout(q2countDown,3000);
+	// countdown begins to answer question #2
+	var hide2=setTimeout(q2countDown,3000);
 	function q2countDown(){
 	$(".question2").hide();
 	$(".question3").show();
-
+	//countdown begins to answer question #3
 	var hide3=setTimeout(q3countDown,3000);
 	function q3countDown(){
 	$(".question3").hide();
 	$(".question4").show();
+	//countdown begins to answer question #4
+	var hide3=setTimeout(q4countDown,3000);
+	function q4countDown(){
+	$(".question4").hide();
+	//code to display code
+	$("#score").show();
+	
 
+
+
+}
 
 }
 
@@ -43,26 +54,18 @@ var hide2=setTimeout(q2countDown,3000);
 }
 
 
+//this is the timer function
+function timer(){
 
-// hide #question 2
-// var hide2=setTimeout(q2countDown,3000);
-// 	function q2countDown(){
-// 	$(".question2").hide();
-// 	//$(".question3").show();
-// 		//alert("testing2");
-// }	
+	var count =5;
 
+	setTimeout(function(){
 
-// var hide3=setTimeout(q3countDown,3000);
-// 	function q3countDown(){
-		
-// 	$(".question3").show();
-// 	//$(".question4").show();
-// 	alert("testing2");
+		count--;
+	$(".timer").text(count);
+	},5000)
 
-
-// 	}
-
+}
 
 
 
