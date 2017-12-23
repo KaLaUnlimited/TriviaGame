ae93c0cc6279f49f5ec0a04f6ed74fa9bc21cd1d
KaLaUnlimited <KLasode@gmail.com>
2017-12-23 12:33:29 -0500

    rearrange classes

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -9,21 +9,23 @@ var wrong;
 var right;
 var noAnswer;
 var number2 =3;
+var currentQuestion;
 
-$(".question1").hide();
-$(".question2").hide();
-$(".question3").hide();
-$(".question4").hide();
-$("#score").hide();
-$("#wrongAnswer").hide();
-$("#rightAnswer").hide();
-$("#newZealand").hide();
+// $(".question1").hide();
+// $(".question2").hide();
+// $(".question3").hide();
+// $(".question4").hide();
+ $("#score").hide();
+ $(".wrongAnswer").hide();
+ $(".rightAnswer").hide();
+ $("#newZealand").hide();
+ $(".questions").hide();
 
 
 ////start button to start the game
 $("#start").click(function(){
 $(".timer").html("You have : " + number + " seconds!");
-
+currentQuestion=0;
 right=0;
 wrong=0;
 noAnswer=0;
@@ -34,7 +36,7 @@ $("#start").hide();
 //countdown begins to answer question #1
 
 timer();//timer function will hide first question and show answer page
-$(".question1").show()//Question #1 show
+$(".questions").val('1').show()//Question #1 show
 
 //when game button is clicked.....
 
@@ -45,17 +47,17 @@ $(".game").click(function(){
 
 				console.log(selection);
 				//show div that confirms right answer 
-				if(selection.hasClass("right")){
-				$("#rightAnswer").show();
+				if(selection.hasClass("correct")){
+				$(".rightAnswer").val('1').show();
 				//$("#newZealand").show()//giphy
-				$(".question1").hide();
+				$(".questions").val('1').hide();
 				right++;
 				//stop();
 				}
 				//if the user selects the wrong button
 				if  (selection.hasClass("wrongAnswer"))
 				{	
-					$(".question1").hide();
+					$(".questions").val('1').hide();
 					$("#wrongAnswer").show();
 					wrong++;
 				//stops the timer to go to the next question
@@ -68,135 +70,139 @@ $(".game").click(function(){
 				
 
 // // **********countdown begins to answer question #2
-setTimeout(q1countDown,miliseconds);
+// setTimeout(q2countDown,miliseconds);
 
-function q1countDown(){
-	$("#rightAnswer").hide();
-	$(".question1").hide();
-	number =6;
-	timer();
-	$(".question2").show();//show question #2 for 90 seconds
-//if use click any option
-$(".game").click(function(){
-				//if game button equals right answer
-				selection=$(this);
-				console.log(typeof selection);
+// function q2countDown(){
+// 	$("#rightAnswer").hide();
+// 	$(".question1").hide();
+// 	number =6;
+// 	timer();
+// 	$(".question2").show();//show question #2 for 90 seconds
+// //if use click any option
+// $(".game").click(function(){
+// 				//if game button equals right answer
+// 				selection=$(this);
+// 				console.log(typeof selection);
 
-				console.log(selection);
-				//show div that confirms right answer 
-				if(selection.hasClass("right")){
-				$("#rightAnswer").show();
-				//$("#newZealand").show()//giphy
-				$(".question2").hide();
-				right++;
-				stop();
-				}
-				//if the user selects the wrong button
-				if  (selection.hasClass("wrongAnswer"))
-				{	
-					$(".question2").hide();
-					$("#wrongAnswer").show();
-					wrong++;
-					stop();
-				}
+// 				console.log(selection);
+// 				//show div that confirms right answer 
+// 				if(selection.hasClass("right")){
+// 				$("#rightAnswer").show();
+// 				//$("#newZealand").show()//giphy
+// 				$(".question2").hide();
+// 				right++;
+// 				stop();
+// 				}
+// 				//if the user selects the wrong button
+// 				if  (selection.hasClass("wrongAnswer"))
+// 				{	
+// 					$(".question2").hide();
+// 					$("#wrongAnswer").show();
+// 					wrong++;
+// 					stop();
+// 				}
 
 				 	
-		})///
+// 		})///endd of game click function
 
 
-	if(number===0){
-		setTimeout(ShowAnswer,3000);}
+//	if(number===0){
+	//	setTimeout(ShowAnswer,3000);}
 
-
+//}
 	// **********countdown begins to answer question #3
-	setTimeout(q2countDown,miliseconds);//starts timer for #3
-		function q2countDown(){
-			$("#rightAnswer").hide();
-			$(".question2").hide();
-			 number =6;
-			timer();
+	// setTimeout(q3countDown,miliseconds);//starts timer for #3
+	// 	function q3countDown(){
+	// 		$("#rightAnswer").hide();
+	// 		$(".question2").hide();
+	// 		 number =6;
+	// 		timer();
 			
-			$(".question3").show();//show question #3
+	// 		$(".question3").show();//show question #3
 
-			$(".game").click(function(){
-				//if game button equals right answer
-				selection=$(this);
-				console.log(typeof selection);
+	// 		$(".game").click(function(){
+	// 			//if game button equals right answer
+	// 			selection=$(this);
+	// 			console.log(typeof selection);
 
-				console.log(selection);
-				//show div that confirms right answer 
-				if(selection.hasClass("right")){
-				$("#rightAnswer").show();
-				//$("#newZealand").show()//giphy
-				$(".question3").hide();
-				right++;
-				stop();
-				}
-				//if the user selects the wrong button
-				if  (selection.hasClass("wrongAnswer"))
-				{	
-					$(".question3").hide();
-					$("#wrongAnswer").show();
-					wrong++;
-					stop();
-				}
+	// 			console.log(selection);
+	// 			//show div that confirms right answer 
+	// 			if(selection.hasClass("right")){
+	// 			$("#rightAnswer").show();
+	// 			//$("#newZealand").show()//giphy
+	// 			$(".question3").hide();
+	// 			right++;
+	// 			stop();
+	// 			}
+	// 			//if the user selects the wrong button
+	// 			if  (selection.hasClass("wrongAnswer"))
+	// 			{	
+	// 				$(".question3").hide();
+	// 				$("#wrongAnswer").show();
+	// 				wrong++;
+	// 				stop();
+	// 			}
 
 				 	
-		})
-			timer();
+	// 	})
 
-			//***********countdown begins to answer question #4
-			setTimeout(q3countDown,miliseconds);
-				function q3countDown(){
-					number =6;
-					timer();
-					$(".question3").hide();
-					$(".question4").show();
+	// 		timer();
+	// }	
+			
+			
 
-					$(".game").click(function(){
-				//if game button equals right answer
-				selection=$(this);
-				console.log(typeof selection);
+// 			//***********countdown begins to answer question #4
+// 			setTimeout(q4countDown,miliseconds);
+// 				function q4countDown(){
+// 					number =6;
+// 					timer();
+// 					$(".question3").hide();
+// 					$(".question4").show();
 
-				console.log(selection);
-				//show div that confirms right answer 
-				if(selection.hasClass("right")){
-				$("#rightAnswer").show();
-				//$("#newZealand").show()//giphy
-				$(".question4").hide();
-				right++;
-				stop();
-				}
-				//if the user selects the wrong button
-				if  (selection.hasClass("wrongAnswer"))
-				{	
-					$(".question4").hide();
-					$("#wrongAnswer").show();
-					wrong++;
-					stop();
-				}
-			});
+// 					$(".game").click(function(){
+// 				//if game button equals right answer
+// 				selection=$(this);
+// 				console.log(typeof selection);
 
-					//countdown begins to answer question #5
-// 					var hide3=setTimeout(q4countDown,miliseconds);
-// 						function q4countDown(){
-// 							number =6;
-// 							timer();
-// 							$(".question4").hide();
-// 							//code to display code
-// 								score();
-// 								//$("#score").show();
+// 				console.log(selection);
+// 				//show div that confirms right answer 
+// 				if(selection.hasClass("right")){
+// 				$("#rightAnswer").show();
+// 				//$("#newZealand").show()//giphy
+// 				$(".question4").hide();
+// 				right++;
+// 				stop();
+// 				}
+// 				//if the user selects the wrong button
+// 				if  (selection.hasClass("wrongAnswer"))
+// 				{	
+// 					$(".question4").hide();
+// 					$("#wrongAnswer").show();
+// 					wrong++;
+// 					stop();
+// 				}
+// 			});
+
+// 					//countdown begins to answer question #5
+// // 					var hide3=setTimeout(q4countDown,miliseconds);
+// // 						function q4countDown(){
+// // 							number =6;
+// // 							timer();
+// // 							$(".question4").hide();
+// // 							//code to display code
+// // 								score();
+// // 								//$("#score").show();
 	
 
 
 
+// // }
+
 // }
 
-}
 
-}	
 
-}
+
 
 
 //////end of start button semi-colon
@@ -219,6 +225,7 @@ $(".game").click(function(){
 
       if (number === 0) {
       	//setTimeout(ShowAnswer,5000);
+
       	$("#rightAnswer").show();
       	$(".question1").hide();
       //	$(".timer").html("Times up!");
@@ -242,10 +249,11 @@ function score(){
 	$("#score").text("Correct answers=(s): " + right + " Incorrect answer(s): "+ wrong+ " Unanswered answer(s): " + noAnswer );
 }
 
-function ShowAnswer(){
+function ShowAnswer (question){
 	
-
+	question= $(".questions");
 	$("#rightAnswer").show();
+	$(this).hide();
 }
 
 
